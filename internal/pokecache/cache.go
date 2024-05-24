8ad4ee4bce8305d7cda1b3cbf6aaca8d77e58627
iferdel <ignaferdel@gmail.com>
2024-05-24 19:36:23 -0400

    Add con empty string error handling

--- internal/pokecache/cache.go
+++ internal/pokecache/cache.go
@@ -1,13 +1,21 @@
 package pokecache
 
+import (
+	"fmt"
+)
+
 type Cache map[string][]byte
 
 func NewCache() Cache {
 	return Cache{}
 }
 
-func (c Cache) Add(key string, value []byte) {
+func (c Cache) Add(key string, value []byte) error {
+	if key == "" {
+		return fmt.Errorf("cannot add an empty key")
+	}
 	c[key] = value
+	return nil
 }
 
 func (c Cache) Get(key string) ([]byte, bool) {
