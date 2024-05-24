420f45a7e9f11f59bd9e12e66cf44d0a6d5a65c5
iferdel <ignaferdel@gmail.com>
2024-05-24 19:06:08 -0400

    Add and Get tests pass

--- internal/pokecache/cache_test.go
+++ internal/pokecache/cache_test.go
@@ -19,8 +19,7 @@ func TestAddGet(t *testing.T) {
 	for i, c := range cases {
 		t.Run(fmt.Sprintf("test case %v", i), func(t *testing.T) {
 			cache := NewCache()
-			// cache.Add(c.key, c.val)
-			cache.Add(c.key, []byte("not"))
+			cache.Add(c.key, c.val)
 
 			got, ok := cache.Get(c.key)
 			if !ok {
