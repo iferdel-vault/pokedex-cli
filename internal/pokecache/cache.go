159415267d8c055bb430ea38222fca4e0c20d2eb
iferdel <ignaferdel@gmail.com>
2024-05-25 06:16:34 -0400

    mutex pointer in Cache definition + reapLoop

--- internal/pokecache/cache.go
+++ internal/pokecache/cache.go
@@ -8,7 +8,7 @@ import (
 
 type Cache struct {
 	cache map[string]cacheEntry
-	mutex *sync.Mutex
+	mutex sync.RWMutex
 }
 
 type cacheEntry struct {
@@ -19,6 +19,7 @@ type cacheEntry struct {
 func NewCache(interval time.Duration) *Cache {
 	c := &Cache{
 		cache: make(map[string]cacheEntry),
+		mutex: sync.RWMutex{},
 	}
 	go c.reapLoop(interval)
 	return c
@@ -28,6 +29,10 @@ func (c *Cache) Add(key string, value []byte) error {
 	if key == "" {
 		return fmt.Errorf("cannot add an empty key")
 	}
+
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
 	cacheEntry := cacheEntry{
 		createdAt: time.Now(),
 		val:       value,
@@ -37,17 +42,28 @@ func (c *Cache) Add(key string, value []byte) error {
 }
 
 func (c *Cache) Get(key string) ([]byte, bool) {
+	c.mutex.RLock()
+	defer c.mutex.RUnlock()
 
 	entry, ok := c.cache[key]
 	return entry.val, ok
 }
 
-/*
 func (c *Cache) reapLoop(interval time.Duration) {
-    ticker := time.NewTicker(time.Second)
-    defer ticker.Stop()
-    for range ticker.C {
-        time.Now()
-    }
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
+	for range ticker.C {
+		currentTime := time.Now()
+
+		c.mutex.Lock()
+
+		for key, entry := range c.cache {
+			if currentTime.Sub(entry.createdAt) > interval { // Sub method returns diff
+				delete(c.cache, key)
+			}
+		}
+
+		// más eff que usar defer al estár dentro de un loop y solicitarse mucho  el lock/unlock
+		c.mutex.Unlock()
+	}
 }
-*/
