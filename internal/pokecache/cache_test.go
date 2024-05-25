84fe76dc89daceb3c0ae094ca9d4080650536f4e
iferdel <ignaferdel@gmail.com>
2024-05-25 19:25:53 -0400

    testing cache map not nil

--- internal/pokecache/cache_test.go
+++ internal/pokecache/cache_test.go
@@ -6,6 +6,14 @@ import (
 	"time"
 )
 
+func TestCreateCache(t *testing.T) {
+	const interval = 5 * time.Second
+    cache := NewCache(interval)
+    if cache.cache == nil {
+        t.Error("shouldn't return nil cache map")
+    }
+}
+
 func TestAddGet(t *testing.T) {
 	const interval = 5 * time.Second
 	cases := []struct {
