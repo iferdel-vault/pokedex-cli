a0d6d742cfd5ec0c8258fb68a937b271f33f1ab8
iferdel <ignaferdel@gmail.com>
2024-05-26 15:34:54 -0400

    mutex to cache

--- internal/pokecache/cache.go
+++ internal/pokecache/cache.go
@@ -2,11 +2,13 @@ package pokecache
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
 type Cache struct {
 	cache map[string]cacheEntry
+	mux   *sync.Mutex
 }
 
 type cacheEntry struct {
@@ -17,12 +19,16 @@ type cacheEntry struct {
 func NewCache(interval time.Duration) Cache {
 	c := Cache{
 		cache: make(map[string]cacheEntry),
+		mux:   &sync.Mutex{},
 	}
 	go c.reapLoop(interval)
 	return c
 }
 
 func (c *Cache) Add(key string, value []byte) error {
+	c.mux.Lock()
+	defer c.mux.Unlock()
+
 	if key == "" {
 		return fmt.Errorf("cannot add an empty key")
 	}
@@ -36,12 +42,15 @@ func (c *Cache) Add(key string, value []byte) error {
 }
 
 func (c *Cache) Get(key string) ([]byte, bool) {
+	c.mux.Lock()
+	defer c.mux.Unlock()
+
 	entry, ok := c.cache[key]
 	return entry.val, ok
 }
 
 func (c *Cache) reapLoop(interval time.Duration) {
-	ticker := time.NewTicker(time.Second)
+	ticker := time.NewTicker(interval)
 	defer ticker.Stop()
 	for range ticker.C { // runs every interval (i.e. interval = 5 min, runs every 5 min)
 		c.reap(interval)
@@ -49,6 +58,9 @@ func (c *Cache) reapLoop(interval time.Duration) {
 }
 
 func (c *Cache) reap(interval time.Duration) {
+	c.mux.Lock()
+	defer c.mux.Unlock()
+
 	currentTime := time.Now().UTC()
 	for key, entry := range c.cache {
 		if currentTime.Sub(entry.createdAt) > interval { // Sub method returns diff
