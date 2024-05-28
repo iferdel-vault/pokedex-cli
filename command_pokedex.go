99ea7e4d8d6206ff120f1be56c2dea4779ab8405
iferdel <ignaferdel@gmail.com>
2024-05-27 20:33:40 -0400

    PERSISTENT DATA STORAGE IN LOCALFILE

--- command_pokedex.go
+++ command_pokedex.go
@@ -1,8 +1,34 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
 )
+
+func loadPokedex(cfg *config, filename string) error {
+	dat, err := os.ReadFile(filename)
+	if err != nil {
+		return fmt.Errorf("error reading file")
+	}
+	err = json.Unmarshal(dat, &cfg.Pokedex)
+	return nil
+}
+
+func savePokedex(cfg *config, filename string) error {
+	data, err := json.MarshalIndent(cfg.Pokedex, "", "  ")
+	if err != nil {
+		return err
+	}
+	if f, err := os.Create(filename); err == nil {
+		defer f.Close()
+		_, err := f.Write(data)
+		if err != nil {
+			return fmt.Errorf("error saving data to pokedex before exiting CLI")
+		}
+	}
+	return nil
+}
 
 func commandPokedex(cfg *config, args ...string) error {
 	fmt.Println("Your Pokedex:")
